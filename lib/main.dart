6fb182b6b427b3e3e13ba0bbdb5fd05dcf4a7d0a
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2024-09-02 19:22:05 +0100

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text("My Coffee Card"),
+        backgroundColor: Colors.brown[700],
+      ),
+      body: const Text("hello, ninja!"),
+    ),
+  ));
+}
